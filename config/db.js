ed7070da179116821e3e14b2bcbf488f33666e0d
Faroukkawu <farukkawujr@gmail.com>
2024-12-13 19:41:28 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://farukkawujr:13fM9G8Xj0XtpE1i@cluster0.01pmx.mongodb.net/lessons?retryWrites=true&w=majority&appName=Cluster0', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB Connected');
     } catch (err) {
         console.error('Database connection error:', err.message);
